e046360a440acfba5b881e9a6715564efb966ff2
João Scheleder Neto <me@scheleder.com>
2024-06-20 21:46:13 -0300

    implementado isProcessing e Error

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,71 +1,48 @@
-import React from 'react'
-import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import api from '@/services/config';
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNzE2ODU4MjYyfQ.ZkFHaoiYCTX52O2OL9UYJNRX8M0-izD7OtULQEr6rx4';
-
-const api = axios.create({
-  baseURL: 'https://estoque.scheleder.com/users',
-});
-
-// Adiciona um interceptor que será executado antes de cada requisição
-api.interceptors.request.use(
-  config => {
-    config.headers.Authorization = `Bearer ${token}`;
-    config.headers.Accept = '*/*';
-    return config;
-  },
-  error => {
-    return Promise.reject(error);
-  }
-);
-
-const Users = (props) => {
+const Users = () => {
   const [data, setData] = useState([]);
   const [isProcessing, setIsProcessing] = useState(true);
   const [error, setError] = useState(null);
 
-
   const getUsers = async () => {
     try {
-      await api.get()
-        .then((response) => {
-          setData(response.data)
-          console.log(response.data)
-        })
-        .catch((err) => {
-          setError(err);
-          console.log(error)
-        })
-        .finally(() => {
-          setIsProcessing(false);
-        })
-    } catch (error) {
-      return (<p>{error}</p>)
+      setIsProcessing(true);
+      const response = await api.get('users');
+      setData(response.data);
+      console.log(response.data);
+    } catch (err) {
+      setError(err);
+      console.log(err);
+    } finally {
+      setIsProcessing(false);
     }
-  }
+  };
 
   useEffect(() => {
     getUsers();
   }, []);
 
-
-
   return (
-
     <div className="pl-16 pt-20">
-      <h2>
-        Users
-      </h2>        
-        <ul className='pl-2'>
+      <h2>Users</h2>
+    
+      {isProcessing ? (
+        <div className="loading">Carregando Dados...</div>
+      ) : error ? (
+        <div className="error">Erro ao carregar os dados: {error.message}</div>
+      ) : (
+        <ul className="pl-2">
           {data.map((dt, index) => (
-            <li key={index}>{dt.id} <br /> {dt.name} <br /> {dt.email}<br /><br /></li>
+            <li key={index}>
+              {dt.id} <br /> {dt.name} <br /> {dt.email} <br /><br />
+            </li>
           ))}
         </ul>
-  
+      )}
     </div>
+  );
+};
 
-  )
-}
-
-export default Users
+export default Users;
---
+++ src/services/config.jsx
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNzE2ODU4MjYyfQ.ZkFHaoiYCTX52O2OL9UYJNRX8M0-izD7OtULQEr6rx4';
+
+const api = axios.create({
+  baseURL: 'https://estoque.scheleder.com/',
+});
+
+// Adiciona um interceptor que será executado antes de cada requisição
+api.interceptors.request.use(
+    config => {
+      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Accept = '*/*';
+      return config;
+    },
+    error => {
+      return Promise.reject(error);
+    }
+  );
+
+  export default api
