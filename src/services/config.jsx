502bc11a04798a45163e43e981e8847437b0ca6f
João Scheleder Neto <me@scheleder.com>
2024-06-25 22:18:41 -0300

    API TO AWS

--- src/services/config.jsx
+++ src/services/config.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNzE2ODU4MjYyfQ.ZkFHaoiYCTX52O2OL9UYJNRX8M0-izD7OtULQEr6rx4';
 
 const api = axios.create({
-  baseURL: 'https://estoque.scheleder.com/',
+  baseURL: 'http://100.29.104.33/',
 });
 
 // Adiciona um interceptor que será executado antes de cada requisição
