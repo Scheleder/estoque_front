721362e387d071068e8ae959dbdc29c968ca064f
João Scheleder Neto <me@scheleder.com>
2024-07-03 22:25:05 -0300

    update

--- src/pages/Components.jsx
+++ src/pages/Components.jsx
@@ -47,8 +47,8 @@ const Components = () => {
                 <tr className="text-xs h-6 text-white text-left uppercase bg-gradient-to-r from-blue-950 to-lime-400">
                   <th className='pl-2'>Categoria</th>
                   <th>Fabricante</th>
-                  <th>SKU</th>
                   <th>Descrição</th>
+                  <th>SKU</th>
                   <th></th>
                 </tr>
               </thead>
@@ -58,8 +58,8 @@ const Components = () => {
                     <tr key={index} className='odd:bg-stone-300 even:bg-stone-200 hover:bg-blue-100 font-semibold'>
                       <td className='px-2 py-1'>{dt.Category.name}</td>
                       <td className='p-1'>{dt.Brand.name}</td>
-                      <td className='p-1'>{dt.sku}</td>
                       <td className='p-1'>{dt.description}</td>
+                      <td className='p-1'>{dt.sku}</td>
                       <td className='p-1'>
                         <Link to={`/components/${dt.id}`}><Eye className='w-4 h-4 text-green-800 hover:text-green-500' /></Link>
                       </td>
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,16 @@
+import { CircleX } from 'lucide-react'
 import React from 'react'
 
-const ErrorPage = ({error}) => {
+const ErrorPage = ({ error }) => {
   return (
-    <div className="pl-16 pt-20">
-      <div className="error">Erro ao carregar os dados: {error.message}
+
+      <div className="fixed z-0 bottom-0 align-middle border-2 border-red-700 rounded-lg bg-gray-300 p-4 w-full m-2">
+          <CircleX className='text-red-700 mr-2 mt-1' /> 
+        <span className='font-semibold text-xl text-red-700'>
+          {error.message}
+        </span>
       </div>
-    </div>
+
   )
 }
 
