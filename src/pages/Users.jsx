f2fbb515ab8ccd3e7113d4126d15f0374d7f3027
João Scheleder Neto <me@scheleder.com>
2024-07-18 20:05:00 -0300

    admin in card user

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -47,10 +47,11 @@ const Users = () => {
                 </Link>
               </div>
               <div className='col-span-2'>
-                <div className='h-12 flex items-end'><span className='text-blue-400 font-semibold'>{dt.name}</span></div>
+                <div className='h-8 flex items-end'><span className='text-blue-400 font-semibold'>{dt.name}</span></div>
                     <a href={`mailto:${dt.email}`} target="_blank" rel="noopener noreferrer">
                       <span className='text-orange-400 hover:text-orange-300 text-xs italic'>{dt.email}</span>
                     </a>
+                    {dt.admin ? <p className='text-lime-500 text-xs font-bold'>Administrador do sistema</p> : ''}
               </div>
               <div className='col-span-3 h-6 text-center'><span className='text-gray-400 text-xs'>Movimentações de estoque nos ultimos 30 dias: {dt.Movements.length}</span></div>
             </li>
